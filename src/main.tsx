615707ce62ccc6efa7adc5b836e87bc987057c86
Denis Sales <denisales@hotmail.com>
2023-12-14 14:46:30 -0300

    first

--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,6 @@ import App1 from "app1/app";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App1 />,
-//   },
-//   {
-//     path: "/detail",
-//     element: <Detail />,
-//   },
-// ]);
-
-console.log(App1);
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     {/* <RouterProvider router={router} /> */}
